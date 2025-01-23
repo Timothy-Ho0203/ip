f13d82fb2d0adbdf9c6b279c630ee61848dfedaf
Timothy-Ho0203 <156638648+Timothy-Ho0203@users.noreply.github.com>
2025-01-24 00:29:16 +0800

    refactor: Added 3 classes to extend Task class for specific behaviour

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
@@ -16,18 +17,37 @@ public class App {
         while (run) {
             String in = input.nextLine();
             String[] instr = in.split(" ");
-            if (instr[0].equalsIgnoreCase("bye")) {
-                run = false;
-                exit();
-            } else if (instr[0].equalsIgnoreCase("list")) {
-                displayList();
-            } else if (instr[0].equalsIgnoreCase("mark")) {
-                markAsDone(Integer.parseInt(instr[1]));
-            } else if (instr[0].equalsIgnoreCase("unmark")) {
-                markAsUndone(Integer.parseInt(instr[1]));
-            } else {
-                addToList(instr[0]);
+            String task = instr[0].toLowerCase();
+            switch (task) {
+                case "bye":
+                    run = false;
+                    exit();
+                    break;
+                case "list":
+                    displayList();
+                    break;
+                case "mark":
+                    markAsDone(Integer.parseInt(instr[1]));
+                    break;
+                case "unmark":
+                    markAsUndone(Integer.parseInt(instr[1]));
+                    break;
+                default:
+                    addToList(in);
+
             }
+//            if (instr[0].equalsIgnoreCase("bye")) {
+//                run = false;
+//                exit();
+//            } else if (instr[0].equalsIgnoreCase("list")) {
+//                displayList();
+//            } else if (instr[0].equalsIgnoreCase("mark")) {
+//                markAsDone(Integer.parseInt(instr[1]));
+//            } else if (instr[0].equalsIgnoreCase("unmark")) {
+//                markAsUndone(Integer.parseInt(instr[1]));
+//            } else {
+//                addToList(instr[0].toLowerCase());
+//            }
         }
     }
     private void greet() {
@@ -50,17 +70,44 @@ public class App {
     }
 
     private void addToList(String task) {
-        list.add(new Task(task));
+        Task curr = null;
+        String type = task.split(" ")[0];
+        switch(type) {
+            case "todo": {
+                String name = task.substring(5);
+                curr = new ToDo(name);
+                break;
+            }
+            case "deadline": {
+                String[] arr = task.split("/");
+                String name = arr[0].substring(9);
+                String date = arr[1].substring(3);
+                curr = new Deadline(name, date);
+                break;
+            }
+            case "event": {
+                String[] arr = task.split("/");
+                String name = arr[0].substring(6);
+                String start = arr[1].substring(5);
+                String end = arr[2].substring(3);
+                curr = new Event(name, start, end);
+                break;
+            }
+        }
+        list.add(curr);
         String base = """
-            Added: %s
+            Added:
+            %s
+            You have %d tasks in the list.
         """;
-        String result = String.format(base, task);
+        int size = list.size();
+        String result = String.format(base, curr, size);
         System.out.print(result + LINE);
     }
 
     private void displayList() {
         int count = 1;
-        StringBuilder sb = new StringBuilder("Here are the tasks you have: \n");
+        StringBuilder sb = new StringBuilder("    Here are the tasks you have: \n");
         String base = "%d. ";
         for (Task task : list) {
             String result = String.format(base, count);
