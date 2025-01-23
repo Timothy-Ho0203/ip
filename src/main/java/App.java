832f5e9b5b1531034eb410e6895fbc994d4d8ccf
Timothy-Ho0203 <156638648+Timothy-Ho0203@users.noreply.github.com>
2025-01-23 12:05:43 +0800

    feat: add feature for bot to echo instructions

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,36 @@
-
+import java.util.Scanner;
 public class App {
     public void run() {
         greet();
-        exit();
+        Scanner input = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            String instr = input.nextLine();
+            if (instr.equals("bye")) {
+                run = false;
+                exit();
+            } else {
+                String line = """
+                    -------------------------------
+                """;
+                System.out.println("    " + instr + "\n" + line);
+            }
+        }
     }
     private void greet() {
         String greetings = """
-        -------------------------------
-        Hello! Welcome to TommyTalks
-        What can i do for you?
-        -------------------------------
+            -------------------------------
+            Hello! Welcome to TommyTalks
+            What can i do for you?
+            -------------------------------
         """;
         System.out.print(greetings);
     }
 
     private void exit() {
         String bye = """
-        Bye, hope to see you again!
-        -------------------------------
+            Bye, hope to see you again!
+            -------------------------------
         """
         ;
         System.out.println(bye);
