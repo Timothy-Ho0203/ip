fd4aed67769c08536c1868a88abfe041cad56f17
Timothy-Ho0203 <156638648+Timothy-Ho0203@users.noreply.github.com>
2025-01-30 17:17:17 +0800

    Refactor App for more OOP
    There is repeated and similar code when determining input commands.
    
    Let's create a seperate Command class handle the actions outside of App
    class to imrpove readability of code.

---
+++ src/main/java/InvalidCommand.java
@@ -0,0 +1,12 @@
+public class InvalidCommand extends Command {
+    protected String inst;
+
+    public InvalidCommand(String inst) {
+        this.inst = inst;
+    }
+
+    @Override
+    public void execute(Storage taskList, Ui ui) {
+        ui.invalidCommand();
+    }
+}
--- src/main/java/TommyTalks.java
+++ src/main/java/TommyTalks.java
@@ -1,15 +1,8 @@
 public class TommyTalks {
     public static void main(String[] args) {
-        String logo = """
-                ___________                           ___________       __   __           \s
-                \\__    ___/___   _____   _____ ___.__.\\__    ___/____  |  | |  | __  ______
-                  |    | /  _ \\ /     \\ /     <   |  |  |    |  \\__  \\ |  | |  |/ / /  ___/
-                  |    |(  <_> )  Y Y  \\  Y Y  \\___  |  |    |   / __ \\|  |_|    <  \\___ \\\s
-                  |____| \\____/|__|_|__/__|_|__/ ____|  |____|  (______/____/__|_|\\/____  >
-                                               \\/                                       \\/\s
-                """;
-        System.out.println("Hello from\n" + logo);
         App chatBot = new App();
         chatBot.run();
     }
+
+
 }
