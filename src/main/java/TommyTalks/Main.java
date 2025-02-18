75f9250a0a3bf55d9b52f55e25b5d209277b1098
Timothy-Ho0203 <156638648+Timothy-Ho0203@users.noreply.github.com>
2025-02-18 15:32:06 +0800

    Add Ui.png to docs

--- src/main/java/TommyTalks/Main.java
+++ src/main/java/TommyTalks/Main.java
@@ -18,6 +18,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Welcome to TommyTalks");
             fxmlLoader.<MainWindow> getController().setTommy(tommmy);  // inject the Duke instance
             stage.show();
         } catch (IOException e) {
